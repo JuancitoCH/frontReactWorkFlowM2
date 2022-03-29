2f48eafe2a2ded9148d5798e8409777116f89874
JuancitoCH <cjuan.chona@hotmail.com>
2022-03-28 23:12:39 -0300

    workflow app  v1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,16 @@ export default function Header() {
     const [headerShow,setHeaderShow] = useState(false)
     const navigate = useNavigate()
     const Dispatch = useDispatch()
+    const errorImg=(e)=>{
+      console.log(e)
+      e.target.style = "display:none"
+      return e.target.onError=null
+  }
+  const onErrorImage = (e) => {
+    e.target.src = "https://images.unsplash.com/photo-1648497400258-7bfd166dfa8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+    e.target.onError = null
+  }
+  
   return (
     <>
       <button className={`fixed left-0 text-3xl text-grosa1-500 z-50`} onClick={()=>setHeaderShow(!headerShow)}><BiArrowToRight/></button>
@@ -24,7 +34,7 @@ export default function Header() {
         : <button className='hover:text-gverdeaqua-900' onClick={()=>{Dispatch(logoutUser());navigate('/')}}>Log out</button>
       }
         <div className={`bg-grosa1-50 rounded-full w-10 h-10 overflow-hidden mx-auto`} >
-          {logged&&<img src={userPhoto}/>}
+          {logged&&<img src={userPhoto} onError={errorImg}/>}
         </div>
         {logged&&<p className={`text-grosa1-50 font-bold`}>{name}</p>}
    
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 export default function Home() {
   return (
     <main className='relative text-white w-[calc(100%-2rem)] min-h-[calc(100vh-2rem)] mt-1 mb-2 bg-gmorado-500 rounded-sm shadow-xl'>
-      <h1 className=' text-4xl absolute left-0'>Workflow App</h1>
+      <h1 className=' text-4xl absolute left-0'>Notion?nonno WorkflowApp</h1>
+      
       {/* w-[calc(100%-4rem)] */}
-     
+     <img className='opacity-5' src="https://images.unsplash.com/file-1646172372557-6258c0de0873image" alt="" />
     </main>
   )
 }
